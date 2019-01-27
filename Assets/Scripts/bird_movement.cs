e78b0db00921bf4cd1c15a21014561f2b7c8c4f3
OoriLaptop <pixel.oori@gmail.com>
2019-01-27 12:54:03 +0800

    WIP bird script + bird positions

---
+++ Assets/Scripts/bird_movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird_movement : MonoBehaviour
+{
+    public List<Transform> hopSpots;
+    public Transform broodingPos;
+
+    [SerializeField]
+    private bool inNest = false;
+    private Animator anim_control;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inNest = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Animation sets
+        if (!inNest)
+        {
+        }
+        else
+        {
+        }
+    }
+}
